4e6d1f95e75a8d525cb174a73df58c2cdef77713
Ariel Mattos <arielmattosnev@gmail.com>
2023-06-12 12:49:33 -0300

    feat: add index to folders to make the import cleaner

---
+++ components/index.ts
@@ -0,0 +1,25 @@
+import Hero from "@/components/Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManuFacturer from "./SearchManuFacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import SearchButton from "./SearchButton";
+import ShowMore from "./ShowMore";
+
+export {
+  Hero,
+  CustomButton,
+  Footer,
+  Navbar,
+  SearchBar,
+  CustomFilter,
+  SearchManuFacturer,
+  CarCard,
+  CarDetails,
+  SearchButton,
+  ShowMore,
+};
